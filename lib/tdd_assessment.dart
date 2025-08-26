5736d238ecf4f941a43210b366ad3f9729643c1c
Sagar Mahobia <sagarmahobia5@gmail.com>
2025-08-26 06:28:24 +0000

    single number returns the number itself [Passing]

--- lib/tdd_assessment.dart
+++ lib/tdd_assessment.dart
@@ -2,7 +2,8 @@ class StringCalculator {
   int add(String number) {
     if (number.isEmpty) {
       return 0;
-    } else
-      return 0;
+    } else {
+      return int.parse(number);
+    }
   }
 }
