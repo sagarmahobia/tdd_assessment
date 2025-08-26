4441acbadf7b5cff72c9dfb2a4962fbadb3203bd
Sagar Mahobia <sagarmahobia5@gmail.com>
2025-08-26 10:45:05 +0530

    Empty project

---
+++ bin/tdd_assessment.dart
@@ -0,0 +1,5 @@
+import 'package:tdd_assessment/tdd_assessment.dart' as tdd_assessment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdd_assessment.calculate()}!');
+}
---
+++ test/tdd_assessment_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_assessment/tdd_assessment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
