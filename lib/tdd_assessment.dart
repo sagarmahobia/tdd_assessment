de59b10f113e63d667ed1f0fd921e85561465c0f
Sagar Mahobia <sagarmahobia5@gmail.com>
2025-08-26 06:40:38 +0000

    negative numbers throw an exception with message having those numbers [Passing]

--- lib/tdd_assessment.dart
+++ lib/tdd_assessment.dart
@@ -4,11 +4,24 @@ class StringCalculator {
       return 0;
     }
 
-    var total = number
-        .split(',')
-        .map((e) => int.parse(e))
-        .fold(0, (e, i) => e + i);
+    var allNumbers = number.split(',').map((e) => int.parse(e));
 
-    return total;
+    var negativeNumbers = [];
+
+    var total = 0;
+    for (int number in allNumbers) {
+      if (number > 0) {
+        total = total + number;
+      } else {
+        negativeNumbers.add(number);
+      }
+    }
+
+    if(negativeNumbers.isNotEmpty){
+    
+      throw FormatException("negative numbers not allowed ${negativeNumbers.join(", ")}");
+    }else{
+      return total;
+    }
   }
 }
